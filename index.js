52589c4a9e7efe1ec16c32a766f90bc3bc2175c0
Katie <thorburnkatie@gmail.com>
2024-06-16 21:49:26 +1000

    not completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+    return console.log(drivers[1,2])
+}
+returnFirstTwoDrivers();
